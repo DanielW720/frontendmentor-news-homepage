465292a0388e974a8c7af50d133a7412f4119565
DW Desktop <westerlund.da@gmail.com>
2023-04-10 22:26:26 +0200

    nav menu transition sort of working

--- solution/src/components/Content/Content.js
+++ solution/src/components/Content/Content.js
@@ -6,7 +6,7 @@ import TopThreeArticles from './TopThreeArticles';
 const Content = () => {
     return (
         <div className={styles.content}>
-            <img src={web3mobile} />
+            <img src={web3mobile} alt="Web 3" />
             <h1>The Bright Future of Web 3.0?</h1>
             <p>
                 We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
--- solution/src/components/header/Header.js
+++ solution/src/components/header/Header.js
@@ -13,23 +13,39 @@ const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles['w-logo']}>W.</div>
-            {showNav ?
-                <div className={styles['nav-container']}>
-                    <div className={styles["close-icon-container"]}><img src={menuClose} /></div>
-                    <nav>
-                        <ul>
-                            <li>Home</li>
-                            <li>New</li>
-                            <li>Popular</li>
-                            <li>Trending</li>
-                            <li>Categories</li>
-                        </ul>
-                    </nav>
-                </div> :
-                <button onClick={handleShowNavClick}>
-                    <img src={mobileMenu} alt="menu" />
-                </button>}
+            <div
+                className={styles['nav-container']}
+                onClick={(e) => {
+                    // Only call handleShowNavClick if div is clicked, not if nav is clicked
+                    if (e.target === e.currentTarget) {
+                        handleShowNavClick();
+                    }
+                }}
+                style={
+                    {
+                        "left": `${showNav ? '0' : '100%'}`
+                    }
+                }>
+                <nav style={
+                    {
 
+                    }
+                }>
+                    <div className={styles["close-icon-container"]}>
+                        <img src={menuClose} alt="Menu close button" onClick={handleShowNavClick} />
+                    </div>
+                    <ul>
+                        <li>Home</li>
+                        <li>New</li>
+                        <li>Popular</li>
+                        <li>Trending</li>
+                        <li>Categories</li>
+                    </ul>
+                </nav>
+            </div>
+            <button onClick={handleShowNavClick} >
+                <img src={mobileMenu} alt="Menu button" />
+            </button>
         </header>
     );
 }
